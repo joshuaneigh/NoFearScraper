d05ab1f5e9fdb146fc1a59f5c76a3d9229c4f9d0
neighbargerj <neighbargerj@deea.thebuffalogroup.com>
2019-01-15 17:04:49 -0500

    Complete JavaDocs and finish cleaning code format.

--- nofearscraper/src/main/java/com/company/model/NoFearScraper.java
+++ nofearscraper/src/main/java/com/company/model/NoFearScraper.java
@@ -21,15 +21,26 @@ import java.util.*;
 public final class NoFearScraper {
 
     /** Arbitraty name of the key which corresponds to the CSV format of the data headers */
-    private static final String KEY_COLUMNS_NAMES = "COLUMN_NAMES";
+    private static final String KEY_COLUMNS_NAMES;
     /** The String for the HTML table tag */
-    private static final String TAG_TABLE = "table";
+    private static final String TAG_TABLE;
     /** The String for the HTML table body tag */
-    private static final String TAG_TABLE_BODY = "tbody";
+    private static final String TAG_TABLE_BODY;
     /** The String for the HTML table data tag */
-    private static final String TAG_TABLE_DATA = "td";
+    private static final String TAG_TABLE_DATA;
     /** The String for the HTML table row tag */
-    private static final String TAG_TABLE_ROW = "tr";
+    private static final String TAG_TABLE_ROW;
+    /** The literal for the HTML "&nbsp;" character. Defined statically to prevent accidental mis-typing. */
+    private static final char TAB_CHAR;
+
+    static {
+        KEY_COLUMNS_NAMES = "COLUMN_NAMES";
+        TAG_TABLE = "table";
+        TAG_TABLE_BODY = "tbody";
+        TAG_TABLE_DATA = "td";
+        TAG_TABLE_ROW = "tr";
+        TAB_CHAR = '\u00a0';
+    }
 
     /**
      * Deprecated starting 15 January 2019. Scrapes the entirety of the HTML table into a single CSV. Does not consider
@@ -62,7 +73,7 @@ public final class NoFearScraper {
     }
 
     /**
-     * Returns the entirety of the table as a CSV. No consideration of empty rows or sub-tables exists.
+     * Handles the processing of the table into a CSV file. No consideration of empty rows or sub-tables exists.
      *
      * @param theTable the jSoup table of which to parse
      * @return a CSV version of the table as a String
@@ -72,41 +83,49 @@ public final class NoFearScraper {
         final Elements rows = theTable.getElementsByTag(TAG_TABLE_ROW);
         for (final Element row : rows) {
             for (final Element cellData : row.getElementsByTag(TAG_TABLE_DATA)) csv.append('"')
-                    .append(cellData.text().replace('\u00a0',' ').trim()).append('"').append(',');
+                    .append(cellData.text().replace(TAB_CHAR,' ').trim()).append('"').append(',');
             csv.append('\n');
         }
         return csv.toString();
     }
 
-    // TODO: Does not work if the tables don't have a null line between each table
-    // TODO: Table 4 has a row without data as well. How to interpret? Also, table 5 is not preceded with a new line
-    // TODO: Table 7 and 8 do not have a blank row for header data. Wtf people. Did you proofread your report?
+    /**
+     * Handles the processing of the table into a CSV file. Takes sub-tables into consideration and implements some
+     * controls for inconsistent formatting problems commonly observed through the current No-FEAR report as of code
+     * production and as observed through archive.org's WayBackMachine. May not work on other internet networks aside
+     * from the World Wide Web, and may break for future reports of the No-FEAR data.
+     *
+     * @param theTable the jSoup table of which to parse
+     * @return a Map with the table name as the Key (i.e.: "III. Issues of Complaints Filed:") and the CSV of that
+     *      sub-table in a String. The table cells are wrapped in \" characters and are delimited using \' characters.
+     *      The first cell is null in each table, followed by the header of each column. The data follows normally.
+     */
     private static Map<String, String> getSubTables(final Element theTable) {
         final Map<String, String> subTables = new HashMap<>();
         final StringBuilder columnNames = new StringBuilder().append("\"\",");
         final Elements rows = theTable.getElementsByTag(TAG_TABLE_ROW);
         final Elements firstRowData = rows.first().getElementsByTag(TAG_TABLE_DATA);
-        String tableName = firstRowData.remove(0).text();
 
         /* Iterates through first row to find names of columns. Skips the first cell. */
+        String tableName = firstRowData.remove(0).text();
         for (final Element columnName : firstRowData) columnNames.append('\"')
-                .append(columnName.text().replace('\u00a0',' ').trim()).append('\"').append(',');
+                .append(columnName.text().replace(TAB_CHAR,' ').trim()).append('\"').append(',');
         subTables.put(KEY_COLUMNS_NAMES, columnNames.toString());
 
         StringBuilder tableCsv = new StringBuilder();
         boolean newTableFlag = false;
         for (final Element row : rows) {
-            if (row.getElementsByTag(TAG_TABLE_DATA).text().replace('\u00a0', ' ').trim().equals("")) {
-                /* If empty row, set flag so that the next row is handled by the new subtable logic */
-                newTableFlag = true;
+            if (row.getElementsByTag(TAG_TABLE_DATA).text().replace(TAB_CHAR, ' ').trim().equals("")) {
+                newTableFlag = true;  /* If this row is empty, set flag to enter alt logic on next row */
             } else if (newTableFlag) {
                 subTables.put(tableName, tableCsv.toString());
                 tableCsv = new StringBuilder().append(columnNames.toString());
-                tableName = row.getElementsByTag(TAG_TABLE_DATA).first().text().replace('\u00a0', ' ').trim();
+                tableName = row.getElementsByTag(TAG_TABLE_DATA).first().text()
+                        .replace(TAB_CHAR, ' ').trim();
                 newTableFlag = false;
             } else {
                 for (final Element data : row.getElementsByTag(TAG_TABLE_DATA)) tableCsv.append('\"')
-                        .append(data.text().replace('\u00a0', ' ').trim()).append('\"').append(',');
+                        .append(data.text().replace(TAB_CHAR, ' ').trim()).append('\"').append(',');
             }
             tableCsv.append('\n');
         }
@@ -114,6 +133,12 @@ public final class NoFearScraper {
         return subTables;
     }
 
+    /**
+     * Gets and returns the relevant table from the DIA No-FEAR HTML scrape.
+     *
+     * @param theDoc the jSoup Document from which to pull the table
+     * @return the relevant HTML table as a jSoup Element
+     */
     private static Element getTable(final Document theDoc) {
         return Objects.requireNonNull(theDoc)
                 .getElementsByTag(TAG_TABLE).first()
@@ -122,7 +147,8 @@ public final class NoFearScraper {
 
     /**
      * Queries web page for full HTML data and handles potential exceptions.
-     * @return the Document containing the HTML data
+     *
+     * @return the jSoup Document containing the HTML data
      */
     private static Document getWebPage(final String theUrl) {
         try {
