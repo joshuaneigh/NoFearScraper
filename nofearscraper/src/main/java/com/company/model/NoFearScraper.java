c2f2055e79f6a3a334584c69c7068237fd5fe139
neighbargerj <neighbargerj@deea.thebuffalogroup.com>
2019-01-15 16:42:58 -0500

    Add documentation and clean code. No functional changes.

--- nofearscraper/src/main/java/com/company/model/NoFearScraper.java
+++ nofearscraper/src/main/java/com/company/model/NoFearScraper.java
@@ -9,22 +9,52 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.*;
 
-public class NoFearScraper {
+/**
+ * Potentially flexible, but designed and tailored to scrape the DIA's NoFEAR web page. There are some formatting
+ * inconsistencies which prompt further use cases, but are decoupled to the best of my ability. If the utility does not
+ * work as expected, please see archive.org's cache of the Q4 FY 2018 report to ensure that the formatting has not
+ * dramatically changed.
+ *
+ * @version 15 January 2019
+ * @author NeighbargerJ
+ */
+public final class NoFearScraper {
 
+    /** Arbitraty name of the key which corresponds to the CSV format of the data headers */
     private static final String KEY_COLUMNS_NAMES = "COLUMN_NAMES";
+    /** The String for the HTML table tag */
     private static final String TAG_TABLE = "table";
+    /** The String for the HTML table body tag */
     private static final String TAG_TABLE_BODY = "tbody";
+    /** The String for the HTML table data tag */
     private static final String TAG_TABLE_DATA = "td";
+    /** The String for the HTML table row tag */
     private static final String TAG_TABLE_ROW = "tr";
 
-//    @Deprecated
+    /**
+     * Deprecated starting 15 January 2019. Scrapes the entirety of the HTML table into a single CSV. Does not consider
+     * any sub-tables or any context of what the data means. Intended as a proof-of-concept, not as a usable solution.
+     *
+     * @param theUrl the URL from which to read the HTML data
+     * @return the CSV as a String
+     */
+    @Deprecated
     public static String scrape(final String theUrl) {
         final Document doc = getWebPage(theUrl);
         final Element table = getTable(doc);
         return getSubTablesAsCsv(table);
     }
 
-
+    /**
+     * Dynamically scrapes the No-FEAR data from the DIA web page. Does not consider improper formatting, with some
+     * exceptions, including the lack of a null line preceding table 5 and the data not leaving space for headers in
+     * tables 7 and 8. See the archived Q4 FY 2018 report for a visual explanation.
+     *
+     * @param theUrl the URL from which to read the HTML data
+     * @return a Map with the table name as the Key (i.e.: "III. Issues of Complaints Filed:") and the CSV of that
+     *      sub-table in a String. The table cells are wrapped in \" characters and are delimited using \' characters.
+     *      The first cell is null in each table, followed by the header of each column. The data follows normally.
+     */
     public static Map<String, String> scrapeSubTables(final String theUrl) {
         final Document doc = getWebPage(theUrl);
         final Element table = getTable(doc);
@@ -33,7 +63,8 @@ public class NoFearScraper {
     }
 
     /**
-     * Returns the entirety of the table as a CSV. No consideration of empty rows or subtables exists.
+     * Returns the entirety of the table as a CSV. No consideration of empty rows or sub-tables exists.
+     *
      * @param theTable the jSoup table of which to parse
      * @return a CSV version of the table as a String
      */
